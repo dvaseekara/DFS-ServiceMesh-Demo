b8fe7ff213ef8cdb79f1e86ccda4fc494dbe6a91
dvaseekara <53438081+dvaseekara@users.noreply.github.com>
2021-02-10 15:07:20 -0500

    Delete curl_customer_quiet.sh

--- curl_customer_quiet.sh
+++
@@ -1,5 +0,0 @@
-export JUPYTERHUB_USER=user1
-export INGRESS_GATEWAY=$(oc get route istio-ingressgateway -n $JUPYTERHUB_USER-smcp -o 'jsonpath={.spec.host}')
-echo "A load generating script is running in the next step. Ctrl+C to stop"
-while :; do sleep 0.2; curl http://${INGRESS_GATEWAY} &> /dev/null ; done
-
