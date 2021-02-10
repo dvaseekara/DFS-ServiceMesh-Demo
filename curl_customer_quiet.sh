fbd25621821c3000062d0bb756bcf48f9b3b697f
dvaseekara <53438081+dvaseekara@users.noreply.github.com>
2021-02-10 15:07:05 -0500

    Update curl_customer_quiet.sh

--- curl_customer_quiet.sh
+++ curl_customer_quiet.sh
@@ -1,4 +1,3 @@
-oc login --token=N1oBMcMrB8y9TygpU7PAbXMeGcgtuEjbIOL52edur9s --server=https://api.cluster-dfs-3370.dfs-3370.sandbox744.opentlc.com:6443
 export JUPYTERHUB_USER=user1
 export INGRESS_GATEWAY=$(oc get route istio-ingressgateway -n $JUPYTERHUB_USER-smcp -o 'jsonpath={.spec.host}')
 echo "A load generating script is running in the next step. Ctrl+C to stop"
